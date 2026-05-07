c978d562889b521f495abe8d38cbdec52a4d3e77
Ethan Li <fourdimentional31@gmail.com>
2026-05-07 19:33:47 -0400

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from djanko_lib import pyx, heading, paragraph, image, link, style
-file = pyx()
-file.title('Candy')
-file.add(style('picture', ['width: 500px', 'length: 500px']))
-file.add(heading('Candy Pictures'))
-file.add(image('/dango.png', alt='Dango Candy', styles='picture'))
-file.add(heading('YAY!'))
-content = file.compile()
