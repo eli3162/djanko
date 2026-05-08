c6ec7ee7d8ddffb93fbe94fccaf6159099e0d992
Ethan Li <fourdimentional31@gmail.com>
2026-05-08 16:09:28 -0400

    Update 404.py

--- 404.py
+++ 404.py
@@ -1,7 +1,7 @@
 # You can customize this 404 page to your heart's content. The '404.py' page will always be the default 404 page
 from djanko_lib import pyx, heading
 file = pyx()
-file.add(heading('404 Not Found'))
+file.add(heading('404 Error - Not Found'))
 file.add(heading('Site made using djanko', level=4))
 
 content = file.compile()
