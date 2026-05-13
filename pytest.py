a80e2ef18dc6fbd6e43298718257233a3ac8ab96
Ethan Li <fourdimentional31@gmail.com>
2026-05-12 20:09:30 -0400

    Add rotation >:]

--- pytest.py
+++ pytest.py
@@ -2,6 +2,7 @@ from djanko_lib import *
 file = pyx()
 file.title('Pyodide Test')
 file.add(heading('Loading...', id='loading-element'))
+file.add(heading('.', id='sprite'))
 file.add(python(pyscript('djanko_lib_client.py')))
 file.add(python(pyscript('pytest.script.py')))
 
--- pytest.script.py
+++ pytest.script.py
@@ -7,17 +7,27 @@ import math, sys
 
 async def main():
     element = document.getElementById("loading-element")
+    sprite = document.getElementById("sprite")
     element.style.color = "red"
     element.textContent = "Pyodide Loaded"
     element.style.position = 'absolute'
+    sprite.style.position = 'absolute'
     element.style.fontSize = "50px"
     element.style.left = '0px'
     element.style.top = '0px'
-    x = 100
-    y = 100
+    x = 500
+    y = 500
     run_speed = 3
     tracking_interval = 4
+    rotation = 0
+    marker = document.createElement("div")
+    marker.innerHTML = '<p>.</p>'
+    document.body.appendChild(marker)
+    await asyncio.sleep(0.01)
+    marker.style.position = 'absolute'
+
     while True:
+        rotation = rotation + 2
         if key_pressed('c'):
             create_element('<p>p</p>')
         if key_pressed('w'):
@@ -42,8 +52,14 @@ async def main():
             x = x - delta_x
             y = y - delta_y
 
-        element.style.left = f"{x}px"
-        element.style.top = f"{y}px"
+        marker.style.top = y
+        marker.style.left = x
+        element.style.transform = f"rotate({rotation}deg)"
+        #element.style.transition = f"transform {0.01}s ease"
+        element.style.left = f"{x - 374}px"
+        element.style.top = f"{y - 16}px"
+        sprite.style.left = f"{x}px"
+        sprite.style.top = f"{y}px"
         element.style.fontSize = f"{50}px"
         await asyncio.sleep(0.01)
 main()
