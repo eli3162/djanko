89e3a614d087c8c07309d187ae54a875c35c234b
Ethan Li <fourdimentional31@gmail.com>
2026-05-19 09:36:38 -0400

    Add intellasense

--- djanko_lib.py
+++ djanko_lib.py
@@ -7,7 +7,7 @@
 import os
 import python_minifier
 class pyx:
-    html = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><link rel="stylesheet" href="/pyscript_offline/pyscript/core.css" /><script type="module" src="/pyscript_offline/pyscript/core.js"></script></script></head><body></body></html>'
+    html = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><link rel="stylesheet" href="https://eli3162.github.io/djanko/pyscript_offline/pyscript/core.css" /><script type="module" src="https://eli3162.github.io/djanko/pyscript_offline/pyscript/core.js"></script></script></head><body></body></html>'
     def add_style(self, href):
         self.html = self.html.replace('</head>', f'<link rel="stylesheet" href="{href}"></head>')
     def add_script(self, src):
